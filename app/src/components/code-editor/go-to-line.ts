5c13c0a77176ccc97707c7c8fdb5a3adf0025fd4
Rafael Garcia <rafbgarcia@gmail.com>
2025-04-24 16:32:00 -0300

    code editor: move cursor to line number to keep it highlighted

--- app/src/components/code-editor/go-to-line.ts
+++ app/src/components/code-editor/go-to-line.ts
@@ -1,5 +1,9 @@
 import type { EditorView } from "@codemirror/view";
 
+/**
+ * Move the cursor to the `lineNumber` and scroll down a bit
+ * to show the line towards the top of the editor.
+ */
 export function scrollToLine(editor: EditorView, lineNumber: number) {
   const scrollIntoLine = Math.min(lineNumber + 30, editor.state.doc.lines);
 
@@ -8,5 +12,12 @@ export function scrollToLine(editor: EditorView, lineNumber: number) {
       anchor: editor.state.doc.line(scrollIntoLine).from,
     },
     scrollIntoView: true,
+  });
+
+  editor.dispatch({
+    selection: {
+      anchor: editor.state.doc.line(lineNumber).from,
+    },
+    scrollIntoView: false,
   });
 }
